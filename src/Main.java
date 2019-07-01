34a564b489922eabbbdc847b84a3f0cce5c89b5d
TrueXPixels <truexpixels@gmail.com>
2019-06-30 18:39:46 -0700

    Event read support & custom input

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
 /*
 v0.0.1
@@ -13,39 +17,69 @@ Currently:
 
 public class Main {
 
+    private static void write(BufferedWriter writer, String out) throws Exception {
+        writer.write(out + "\r\n");
+        writer.flush();
+    }
+
     public static void main(String[] args) throws Exception {
 
         // Variables
-        String  nickname = "IRCClient_TrueXPixels",
+        String  server = "irc.zoro.moe",
+                nickname = "IRCClient_TrueXPixels",
                 username = "txp",
-                realname = "TrueXPixels",
+                realname = "IRC Client [TrueXPixels]",
                 channel  = "#bots",
                 joinMsg  = "Hello Channel!";
 
+        // User Input
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Server Address: ");
+        server = sc.next();
+        System.out.print("Nickname: ");
+        nickname = sc.next();
+        System.out.print("Channel/User: ");
+        channel = sc.next();
+
+        sc.close();
+
         // Create Socket & Buffers
-        Socket socket = new Socket("irc.zoro.moe", 6667);
+        Socket socket = new Socket(server, 6667);
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
         BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
         // Set Nick & User
-        writer.write("NICK " + nickname + "\r\n");
-        writer.write("USER " + username + " 8 * : " + realname + "\r\n");
-        writer.flush();
+        write(writer, "NICK " + nickname);
+        write(writer, "USER " + username + " 8 * : " + realname);
 
         // Handle Lines
         String line = null;
-        while ((line = reader.readLine()) != null) {
-            if (line.startsWith("PING")) { // Respond to pings to stay connected
-                writer.write("PONG :" + line.substring(6) + "\r\n");
-                writer.flush();
-            } else if (line.contains("004")) { // Runs on connection (Logging In)
-                writer.write("JOIN " + channel + "\r\n"); // Join Channel
-                writer.write("PRIVMSG " + channel + " :" + joinMsg + "\r\n"); // Send Message
-                writer.flush();
-            } else if (line.contains("433")) { // Runs on nickname in use, breaks
-                System.out.println("That nickname is already being used!");
-                break;
+        Boolean destroy = false;
+        while ((line = reader.readLine()) != null || !destroy) {
+
+            // Handle Pings
+            if (line.startsWith("PING")) write(writer, "PONG :" + line.substring(6));
+
+            // Handle Codes
+            String[] content = line.split(" ");
+            switch(content[1]) {
+                case "PRIVMSG":
+                    System.out.println("[" + content[2] + "] " + content[0].split("!")[0].split(":")[1] + ": " + line.split(":")[2]);
+                    break;
+                case "001":
+                    System.out.println("\n" + line.split(":")[2]);
+                    break;
+                case "004":
+                    write(writer, "JOIN " + channel + "\r\n"); // Join Channel
+                    System.out.println("Connected to " + channel + "...\n");
+                    break;
+                case "433":
+                    System.out.println("Sorry, that nickname is already in use.");
+                    destroy = true;
+                    break;
             }
+
         }
 
     }
